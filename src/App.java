92edad21588ea8c625b35af8690902d1045c1dee
jonathanMorochoYu <erickzhigue@gmail.com>
2025-01-29 12:52:59 -0500

    Diccionarios concepto

--- src/App.java
+++ src/App.java
@@ -8,25 +8,37 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // Ejecuta el ejemplo de uso de HashMap con ejemplos sencillos
-        runMapExamlpe();
+        runMapExample();
 
         // Ejecuta el ejemplo de gestión de empleados usando HashMap
         runEmpleadoExample();
 
         // Ejecuta los ejercicios de sumatoria y anagramas
-        runEjerccios();
+        //runEjerccios();
     }
 
+    private static void runMapExample() {
+        new Mapa();
+        System.out.println();
+    }
+    
     private static void runEmpleadoExample() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        System.out.println("*****EJERCICIO DE EMPLEADOS******");
+        EmpleadoContoller empleadoContoller = new EmpleadoContoller();
+        empleadoContoller.addEmpleado(new Empleado(1, "Pablo", "Senior"));
+        empleadoContoller.addEmpleado(new Empleado(2, "Juan", "Junior"));
+        empleadoContoller.addEmpleado(new Empleado(3, "Miguel", "Senior"));
+    
+        empleadoContoller.displayEmpleados();
+        empleadoContoller.displayEmpleadosSoloNombres();
+        empleadoContoller.displayLlaves();
+        empleadoContoller.displayEmpleadosSinSet();
     }
 
-    private static void runMapExamlpe() {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
+    /* 
     private static void runEjerccios() {
         throw new UnsupportedOperationException("Not implemented yet");
 
     }
+        */
 }
--- src/controllers/EmpleadoContoller.java
+++ src/controllers/EmpleadoContoller.java
@@ -7,7 +7,63 @@ import models.Empleado;
 
 public class EmpleadoContoller {
 
-    public EmpleadoContoller() {
+    private Map<Integer, Empleado> empleados;
 
+
+    public EmpleadoContoller() {
+        empleados = new HashMap<>();
+    }
+
+    /*
+     * Ingresar el empleado donde la clave sea el id
+     * @param Empleado a ingresar al mapa
+     * @return False si no ingreso porque ya existe
+     *         True si ingreso correctamente
+     */
+
+    public boolean addEmpleado(Empleado empleado){
+        if(empleados.containsKey(empleado.getId()))
+            return false;
+        
+        empleados.put(empleado.getId(), empleado);
+        return true;
+    }
+
+    /*
+     * Buscar un empleado por su id
+     * @param int id a ingresar al mapa
+     * @return Empleado segun id
+     */
+
+    public Empleado getEmpleadoById(int id){
+        return empleados.get(id);
+    }
+
+    public void displayEmpleados(){
+        System.out.println("Empleados con llaves");
+        for(Map.Entry<Integer, Empleado> empleadoEntry : empleados.entrySet()){
+            System.out.println(empleadoEntry.getKey() +  " - " + empleadoEntry.getValue());
+        }
+    }
+
+    public void displayEmpleadosSoloNombres(){
+        System.out.println("Nombres de empleados: ");
+        for(Map.Entry<Integer, Empleado> empleadoEntry : empleados.entrySet()){
+            System.out.println(empleadoEntry.getKey() +  " - " + empleadoEntry.getValue().getName());
+        }
+    }
+
+    public void displayLlaves(){
+        System.out.println("Llaves:");
+        for(Integer empleadoEntry : empleados.keySet()){
+            System.out.println(empleadoEntry);
+        }
+    }
+
+    public void displayEmpleadosSinSet(){
+        System.out.println("Empleados sin llaves:");
+        for(Empleado empleadoEntry : empleados.values()){
+            System.out.println(empleadoEntry);
+        }
     }
 }
